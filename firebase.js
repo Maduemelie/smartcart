8736110d0b343f74ba5202cc958de627d2c8cde1
maduemelie <maduemelie@gmail.com>
2025-10-03 21:09:15 +0100

    feat(firebase): update Firebase configuration to use environment variables and refactor dependencies

--- firebase.js
+++ firebase.js
@@ -1,18 +1,16 @@
 import { initializeApp } from 'firebase/app';
 // eslint-disable-next-line import/no-unresolved
 import { initializeAuth, getReactNativePersistence } from 'firebase/auth'; // <-- This is the correct import for persistence in Expo
-import Constants from 'expo-constants';
-
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 
 // Your web app's Firebase configuration from environment variables
 const firebaseConfig = {
-  apiKey: Constants.expoConfig.extra.FIREBASE_API_KEY,
-  authDomain: Constants.expoConfig.extra.FIREBASE_AUTH_DOMAIN,
-  projectId: Constants.expoConfig.extra.FIREBASE_PROJECT_ID,
-  storageBucket: Constants.expoConfig.extra.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: Constants.expoConfig.extra.FIREBASE_MESSAGING_SENDER_ID,
-  appId: Constants.expoConfig.extra.FIREBASE_APP_ID,
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Validate the Firebase configuration to prevent runtime errors, especially in preview/production.
